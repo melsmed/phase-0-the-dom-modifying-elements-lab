a6d18531f1577b4d7ae50b9573a10a8d8d858a6d
Melissa Medina <melissa.med52@gmail.com>
2023-06-04 09:41:37 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// const element = document.createElement('div');
+// element.textContent = 'This is a new element';
+
+// // Append the element to the body
+// document.body.append(element)
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+//   element.append(ul);
+document.querySelector("main#main").remove()
+const newHeader = document.createElement("h1");
+newHeader.id= ("victory")
+newHeader.textContent= "Melissa is the champion"
